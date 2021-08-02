798db95f409e1ddc03b895639fdd7309226109f4
Diegbys Mudarra <diegbys.mudarra@grupoleiros.com>
2021-08-02 17:02:06 -0400

    iindividual post

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,10 +1,37 @@
 
 import Layout from "../../components/Layout";
+import Link from 'next/link';
 
-export default function index() {
+export default function index({ data }) {
     return (
         <Layout title="About" description='tu sabe que es la descripcio'>
             <h1>listado de posts</h1>
+            {
+                data.map(({ id, title, body }) => (
+                    <div key={id}>
+                        <h3>
+                            <Link href={`/blog/${id}`}>
+                                <a> {id} - {title}</a>
+                            </Link>
+                        </h3>
+                        <p>{body}</p>
+                    </div>
+                ))
+            }
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json();
+        return {
+            props: {
+                data
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
