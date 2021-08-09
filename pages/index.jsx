615460ad33b941cc9a9e9c22ce7231f0c4a7ab33
Diegbys Mudarra <diegbys.mudarra@grupoleiros.com>
2021-08-09 16:59:01 -0400

    movies

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,39 @@
 import Layout from "../components/Layout";
 import utilStyles from '../styles/utils.module.css'
+import connectDB from '../lib/dbConnect';
+import Movie from '../models/Movie';
 
+export default function Home({ movies }) {
+    console.log(movies);
+    return (
+        <Layout title="Home" description="this is the home" home>
+            <br />
+            {movies.map(item => (
+                <div className="card mb-2" key={item._id}>
+                    <div className="card-body">
+                        <div className="h5 text-uppercase">{item.title}</div>
+                        <p className="fw-light">{item.plot}</p>
+                    </div>
+                </div>
+            ))}
 
-export default function Home() {
-  return (
-    <Layout title="Home" description="this is the home" home>
+        </Layout>
+    )
+}
 
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+export async function getServerSideProps() {
+    try {
+        await connectDB();
 
-    </Layout>
-  )
+        const res = await Movie.find({});
+        const movies = res.map(doc => {
+            let movie = doc.toObject();
+            movie._id = `${movie._id}`;
+            return movie;
+        });
+
+        return { props: { movies } }
+    } catch (error) {
+        console.log(error);
+    }
 }
